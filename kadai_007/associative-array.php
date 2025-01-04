891c7e0c3119e1ab13cfe36cf8756132b091aeab
dxryookada <ryookada@tokyo-gas.co.jp>
2025-01-04 20:43:04 +0900

    add_kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 'gender'というキーを持つ値（'男性'）が出力される
+    print_r($personal_data);
+    ?>
+</body>
+</html>
